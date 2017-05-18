aee38f29311db7c9f66e6d3d397ab5c7d76a1673
Alex <alexbestul@users.noreply.github.com>
2017-05-18 16:27:45 -0500

    Follow PowerShell idioms for parameters
    This change maintains the existing usage pattern (as described in the
    README). It adds basic parameter validation, and also allows values to
    be passed in as command-line parameters if desired.

--- createPackage.ps1
+++ createPackage.ps1
@@ -1,6 +1,10 @@
-$version = $env:version
-$revision = $env:revision
-$path = "gocd-" + $args[0]
+param(
+    [string][parameter()][ValidateSet("agent", "server")]$app,
+    [string][parameter()][ValidateNotNullOrEmpty()]$version = $env:version,
+    [string][parameter()][ValidateNotNullOrEmpty()]$revision = $env:revision
+)
+
+$path = "gocd-" + $app
 $fullVersion = $version + "-" + $revision
 cd $path
 (Get-Content tools\chocolateyInstall.ps1.template) -replace '{{fullVersion}}', $fullVersion | out-file tools\chocolateyInstall.ps1;
